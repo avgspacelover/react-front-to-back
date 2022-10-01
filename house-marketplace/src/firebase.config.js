38cddfb55f9644539a5c6a55b08744e81de2fd7a
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-01 08:17:08 +0530

    adding basic files

---
+++ house-marketplace/src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlcymGz4FEJwIOVzpNUIHDffWa-lE3qSg",
+  authDomain: "react-ftb-house-mktplace.firebaseapp.com",
+  projectId: "react-ftb-house-mktplace",
+  storageBucket: "react-ftb-house-mktplace.appspot.com",
+  messagingSenderId: "1071188562499",
+  appId: "1:1071188562499:web:fc10aa57805bc7f5d9baf6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(); 
