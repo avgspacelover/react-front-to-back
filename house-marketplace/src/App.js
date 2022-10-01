d353b7954350fcfe30f965c55b3011e71613eb1c
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-01 13:46:59 +0530

    form setup UI

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,10 +1,40 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navbar } from './components/Navbar';
+import { Explore } from './pages/Explore';
+import { ForgotPassword } from './pages/ForgotPassword';
+import { Offer } from './pages/Offer';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 
 
 function App() {
   return (
     <div className="App">
-      
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+        
+          <Route path='/offers' element={<Offer />} />
+          
+          <Route path='/profile' element={<SignIn />} />
+
+          <Route path='/sign-in' element={<SignIn />} />
+
+          <Route path='/sign-up' element={<SignUp />} />
+
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+
+        </Routes>
+
+
+        <Navbar />
+
+      </Router>
+
+       
+
     </div>
   );
 }
---
+++ house-marketplace/src/pages/Explore.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Explore = () => {
+  return (
+    <h1>Explore</h1>
+  )
+}
